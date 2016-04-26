c1bad98afe838d61d16106c8b6163560e9456cf3
Nonico Bugash <nonico_bugash@rapid7.com>
2016-04-26 10:25:12 -0400

    Initial commit

---
+++ appspider_notify.js
@@ -0,0 +1,38 @@
+/**
+ * Created by nbugash on 2016-04-26.
+ */
+/**
+ * Created by nbugash on 4/8/16.
+ */
+// request permission on page load
+document.addEventListener('DOMContentLoaded', function () {
+    if (Notification.permission !== "granted"){
+        Notification.requestPermission();
+    }
+});
+window.addEventListener('load', function() {
+    if (/Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor)){
+        notifyPlugin();
+    }
+});
+function notifyPlugin() {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.');
+        return;
+    }
+
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+        var notification = new Notification('AppSpider Validate', {
+            icon: 'images/appspider_notify_icon.png',
+            body: "Hey there! There's a chrome plugin available to open the validate button"
+        });
+
+        notification.onclick = function () {
+            window.open("https://chrome.google.com/webstore/detail/appspider-validate-chrome/decadhidkojjhcbbjhljnofgejilmmkl");
+        };
+
+    }
+
+}
